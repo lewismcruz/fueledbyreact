ed004266549a94ef89e014e7facb32895460fbe2
lewismcruz <lewis.cruz@gmail.com>
2020-07-13 21:51:08 -0500

    Created About.js JSX content in About.js component file.

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+          <h1>About Page</h1>
+          <p>
+            Little text here about myself and interests.
+
+          </p>
+
+          <ul>
+            <li>List Item 1</li>
+            <li>List Item 2</li>
+            <li>List Item 3</li>
+            <li>List Item 4</li>
+
+          </ul>
+        </div>
+    );
+}
+
+export default About;
