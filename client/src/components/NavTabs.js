9f7700d0f2af7658043ddd503bd82951effda8cb
lewismcruz <lewis.cruz@gmail.com>
2020-07-13 22:00:05 -0500

    Created NavTabs.js component

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,13 +1,41 @@
 import React from "react";
 
-function Navbar() {
+function NavTabs(props) {
     return (
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+              <a href="#home" onClick={() => props.handlePageChange("Home")} className="nav-link">
+                Home
+              </a>
+          </li>
+          <li className="nav-item">
+              <a href="#about" onClick={() => props.handlePageChange("About")} className="nav-link">
+                  About
+              </a>
+          </li>
+          <li className="nav=item">
+              <a href="#blog" onClick={() => props.handlePageChange("Blog")} className="nav-link">
+                  Blog
+              </a>
+          </li>
+          <li className="nav-item">
+            <a href="#contact" onClick={() => props.handlePageChange("Contact")} className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+    );
+}
+
+export default NavTabs;
+
+
+//===============================================================
+// Can create a NavBar with this code snippet or stick with nav-tabs
+/*
+ return (
         <navbar className="navbar">
             <div><p>Placeholder Text</p></div>
         </navbar>
     );
-}
-
-export default Navbar;
-
-
+*/
